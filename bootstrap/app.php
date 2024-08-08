36b18af024d3bcfe6704a7843a8fbc140847c1ba
Platov Sergei <serg1764@gmail.com>
2024-08-08 23:10:49 +0300

    task-4 add routing

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         // Регистрация alias для вашего middleware
         $middleware->alias([
             'checkAdmin' => \App\Http\Middleware\CheckAdmin::class,
+            'checkAccount' => \App\Http\Middleware\CheckAccount::class,
             // Можно добавить другие middleware с alias
         ]);
     })
