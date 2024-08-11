099949cb6a7234a1c1521bdae80311a8be20ac4a
Platov Sergei <serg1764@gmail.com>
2024-08-11 15:31:09 +0300

    task-6 add LTEAdmin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,11 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Helper;
 use App\Models\User;
 use Illuminate\Http\Request;
+use JeroenNoten\LaravelAdminLte\AdminLte;
 
 class AdminController extends Controller
 {
+    protected $adminlte;
+
+    public function __construct(AdminLte $adminlte)
+    {
+        Helper::logToDatabase($this->adminlte);
+        $this->adminlte = $adminlte;
+    }
     public function index()
     {
         // Получаем данные, которые нужно передать в представление
@@ -15,6 +24,14 @@ class AdminController extends Controller
         //$postsCount = Post::count();  // Пример получения количества постов
 
         // Возвращаем представление для администрирования
-        return view('admin.index', compact('usersCount', 'postsCount')); // Здесь предполагается, что у вас есть файл resources/views/admin/index.blade.php
-    }
+        //return view('admin.index', compact('usersCount', 'postsCount'));
+        // Здесь предполагается, что у вас есть файл resources/views/admin/index.blade.php
+
+        Helper::logToDatabase($this->adminlte);
+
+        return view('vendor.adminlte.page', [
+            'adminlte' => $this->adminlte,
+            'usersCount' => $usersCount,
+            'postsCount' => $postsCount
+        ]);}
 }
