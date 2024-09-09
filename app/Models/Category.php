749bdb713b851e5ef092159b49c2fee07e5deef8
Platov Sergei <serg1764@gmail.com>
2024-09-09 20:22:05 +0300

    task-9 add a category editing page

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -13,7 +13,7 @@ class Category extends Model
     protected $table = 'categories';
 
     // Указание полей, которые можно массово заполнять
-    protected $fillable = ['*'];
+    protected $fillable = ['parent_id', 'title', 'name', 'description', 'url', 'content', 'imgs', 'inactive'];
 
     // Метод для получения фиксированных данных
 
@@ -51,5 +51,114 @@ class Category extends Model
                 'parent_id' => 2,
             ],
         ]);
+    }
+
+    /**
+     * @description
+     *
+    */
+    public static function getCategory($id): array
+    {
+        $Result = [
+            'data' => [],
+            'success' => false,
+            'error' => ''
+        ];
+
+        try {
+
+            if($id !== 'new') {
+                $Result['data'] = self::findOrFail($id)->toArray();
+            }
+            else{
+                $Result['data'] = [
+                    'id' => 'new',
+                    'url' => '',
+                    'title' => '',
+                    'name' => '',
+                    'description' => '',
+                    'parent_id' => '',
+                    'content' => '',
+                    'imgs' => '',
+                    'inactive' => true,
+                    'created_at' => '',
+                    'updated_at' => '',
+
+                ];
+            }
+            // Получение всех категорий
+            $allCategories = Category::select('id', 'parent_id', 'name')->get()->toArray();
+
+            // Преобразование массива всех категорий в ассоциативный массив по id
+            $categoriesById = [];
+            foreach ($allCategories as $cat) {
+                $categoriesById[$cat['id']] = $cat;
+            }
+
+            // Добавление поля 'parent' в каждый элемент массива
+            foreach ($allCategories as &$cat) {
+                if ($cat['parent_id']) {
+                    $parent = $categoriesById[$cat['parent_id']] ?? null;
+                    if ($parent) {
+                        $cat['parent'] = "prnt: {$parent['id']} - {$parent['name']}";
+                    } else {
+                        $cat['parent'] = "prnt: {$cat['parent_id']} - Unknown";
+                    }
+                } else {
+                    $cat['parent'] = "prnt: none";
+                }
+            }
+
+            $Result['data']['list_all_categories'] = $allCategories;
+            $Result['success'] = true;
+
+        } catch (\Exception $e) {
+            $Result['error'] = 'Category not found - ' . $e->getMessage();
+            Helper::logToDatabase('Category', $e->getMessage(), '$Result');
+        }
+
+        Helper::logToDatabase('Category', $Result, '$Result');
+
+        return $Result;
+    }
+
+    public static function saveCategory($Data): array
+    {
+        $Result = [
+            'data' => [],
+            'success' => false,
+            'error' => ''
+        ];
+
+        try {
+            //dd(self::find($Data['id']));
+
+            $Data['id'] = ($Data['id'] === 'new') ? null : $Data['id'];
+            // Найти категорию по ID или создать новую, если не найдена
+            $category = self::updateOrCreate(
+                [
+                   'id' => $Data['id']
+                ], // Условие поиска по ID
+                [
+                    'parent_id' => $Data['parent_id'],
+                    'title' => $Data['title'],
+                    'name' => $Data['name'],
+                    'description' => $Data['description'],
+                    'url' => $Data['url'],
+                    'content' => $Data['content'],
+                    'imgs' => $Data['imgs'],
+                    'inactive' => $Data['inactive']
+                ] // Данные для обновления или создания
+            );
+
+            $Result['data'] = $category->toArray();
+            $Result['incomingData'] = $Data;
+            $Result['success'] = true;
+        } catch (\Exception $e) {
+            $Result['error'] = 'Failed to save category - ' . $e->getMessage();
+            Helper::logToDatabase('Category', $e->getMessage(), '$Result');
+        }
+
+        return $Result;
     }
 }
